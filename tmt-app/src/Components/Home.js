312dbf626ac38334ae8b067afcf12d7b0e517b5f
Raymond0T <36683390+Raymond0T@users.noreply.github.com>
2023-02-02 13:55:01 -0800

    Home page component (#7)

---
+++ tmt-app/src/Components/Home.js
@@ -0,0 +1,128 @@
+import React from "react";
+import '../Styling/Home.css';
+import {PrimaryBtn, FlexContainer} from '../Styling/CustomStyling.js';
+import {Link, Container, Typography} from '@mui/material';
+import ForwardArrow from '../Images/Forward.png';
+import MilkTeaBlob from '../Images/MilkTeaBlob.png';
+import LycheeRectangle from '../Images/LycheeRectangle.png';
+import MilkTeaSwiggle2 from '../Images/MilkTeaSwiggle2.png';
+import ThaiTeaSwiggle1 from '../Images/ThaiTeaSwiggle1.png';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+
+
+
+function Home(){
+    return(
+        <Container 
+            sx={{
+                minWidth:"100%", 
+                paddingTop:"7.5rem", 
+                overflow:"hidden", 
+                minHeight:"100vh"
+            }}
+        >
+            <Container>
+                <Container 
+                    sx={{
+                        width:"100%", 
+                        display:"flex", 
+                        alignItems:"center",
+                        textAlign:"center"
+                    }}
+                >
+                    <Typography variant="CustomSubHeading" sx={{color:"BobaHighlight.main"}}>Student Projects</Typography>
+                    <img id="Home_Path_Arrow"src={ForwardArrow} alt="ForwardArrow"/>
+                    <Typography variant="CustomSubHeading" sx={{color:"BobaHighlight.main"}}>Major Picker</Typography>
+                </Container>
+            </Container>
+
+            <FlexContainer sx={{paddingY:"6.25rem"}}>
+                <Container 
+                    sx={{
+                        position: "absolute",
+                        top: "0",
+                        zIndex: "-3",
+                        minWidth: "1585px",
+                        paddingY: "2rem",
+                    }}>
+                    <img id="Home_Hero_Background" src={MilkTeaBlob} alt="MilkTeaBlob"/>
+                </Container>
+                <FlexContainer sx={{flexDirection:"column", position:"none"}}>
+                    <Container sx={{textAlign:"center", paddingBottom:"1.5rem"}}>
+                        <Typography variant="CustomTitle" sx={{color:"Boba.main"}}>
+                            Can&apos;t decide between majoring in business, biology, or art?
+                        </Typography>
+                    </Container>
+                    <Container sx={{textAlign:"center", marginY:"1.75rem"}} maxWidth="md">
+                        <Typography variant="CustomHeading3">
+                            No one can master multiple subjects the way you do. Spend 2 minutes 
+                            with us to find the perfect major for you!
+                        </Typography>
+                    </Container>
+                </FlexContainer>
+            </FlexContainer>
+
+            <FlexContainer sx={{flexDirection:"row"}}>
+                <FlexContainer 
+                    sx={{
+                        position: "absolute",
+                        bottom:"-125%",
+                        justifyContent: "space-between",
+                        zIndex: "-1",  
+                    }}
+                >
+                    <div className="Home_Button_Bg_Left">
+                        <img 
+                            src={LycheeRectangle} 
+                            id="Home_Button_Bg_Img1" 
+                            alt="LycheeRectangle"
+                        />
+                        <img 
+                            src={MilkTeaSwiggle2} 
+                            id="Home_Button_Bg_Img2" 
+                            alt="MilkTeaSwiggle2"
+                        />
+                    </div>
+                    <div className="Home_Button_Bg_Right">
+                        <img 
+                            src={ThaiTeaSwiggle1} 
+                            id="Home_Button_Bg_Img3" 
+                            alt="ThaiTeaSwiggle1"
+                        />
+                    </div>
+                </FlexContainer>
+                <FlexContainer 
+                    sx={{
+                        flexDirection:"column", 
+                        height:"auto"
+                    }}
+                >
+                    <PrimaryBtn sx={{color: "#FFFFFF"}}> 
+                        <Typography variant="CustomHeading2">
+                            Take the Quiz
+                        </Typography>
+                    </PrimaryBtn>
+ 
+                    <div id="Home_Button_Main_Link">
+                        <Link
+                            sx={{color:"Boba.main"}}
+                            underline="none"
+                            href="google.com" 
+                            target="_blank"
+                            rel="noopener noreferrer" 
+                            aria-label="Learn more about other different majors"
+                        >
+                            <Typography variant="CustomSubHeading">
+                                See all Majors/Careers
+                            </Typography>
+                        </Link>
+                        <ArrowRightAltIcon sx={{paddingTop:"3px", paddingLeft:"10px"}}/>
+                    </div>
+                    
+                </FlexContainer>
+            </FlexContainer>
+        </Container>
+    )
+}
+
+export default Home;
