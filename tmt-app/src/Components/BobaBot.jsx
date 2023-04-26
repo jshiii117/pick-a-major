44f3aa754c6363fd626c0544a3e7425501bc3c8b
James Shi <jamesshi117@gmail.com>
2023-04-26 15:06:00 -0700

    Make Boba Bot more responsive (#100)
    * Add Quiz component title and list
    
    * Add media query to Boba Bot

--- tmt-app/src/Components/BobaBot.jsx
+++ tmt-app/src/Components/BobaBot.jsx
@@ -13,7 +13,12 @@ function BobaBot({ text }) {
       <img
         src={BobaBotLarge}
         alt={`Mascot icon with speech bubble that says ${text}`}
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+        }}
       />
+
       <Box
         sx={{
           alignItems: "center",
@@ -23,13 +28,29 @@ function BobaBot({ text }) {
           left: "1.5rem",
           position: "absolute",
           top: "2rem",
-          width: "70%",
+          width: "50%",
+          "@media (max-width: 2000px)": {
+            left: "1rem",
+            top: "1.5rem",
+            width: "75%",
+          },
+          "@media (max-width: 960px)": {
+            left: "0.8rem",
+            top: "1.2rem",
+            width: "80%",
+          },
         }}
       >
         <Typography
           sx={{
             textAlign: "center",
             overflow: "hidden",
+            "@media (max-width: 960px)": {
+              fontSize: "0.9rem",
+            },
+            "@media (max-width: 600px)": {
+              fontSize: "0.8rem",
+            },
           }}
         >
           {text}
--- tmt-app/src/Components/Quiz.jsx
+++ tmt-app/src/Components/Quiz.jsx
@@ -1,19 +1,83 @@
 import React from "react";
-import { Container, Grid } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { Container, Grid, Typography } from "@mui/material";
+import { CustomTheme } from "../Styling/CustomStyling";
 import QuestionCard from "./QuestionCard";
-import { CAREERS_CARDS_PLACEHOLDERS } from "../constants/careerMajorList";
+import BobaBot from "./BobaBot";
+import {
+  CAREERS_CARDS_PLACEHOLDERS,
+  SOFT_SKILL_LIST,
+} from "../constants/careerMajorList";
 
 function Quiz() {
+  const { step } = useParams();
+
+  const datasets = { 1: CAREERS_CARDS_PLACEHOLDERS, 2: SOFT_SKILL_LIST };
+  const data = datasets[step] || datasets[1];
+
   return (
-    <Container>
-      <Grid container justifyContent="center">
-        {CAREERS_CARDS_PLACEHOLDERS.map((item) => (
+    <Container sx={{ width: "100%" }}>
+      <Text step={step} />
+      <Grid container justifyContent={{ md: "center", lg: "flex-start" }}>
+        {data.map((item) => (
           <Grid item key={item.title}>
             <QuestionCard item={item} />
           </Grid>
         ))}
       </Grid>
     </Container>
+  );
+}
+
+function Text({ step }) {
+  const textContent = {
+    1: {
+      title1: "Let's order some boba! Pick your base(s).",
+      title2: "What university classes sound interesting to you?",
+      subtitle: "Select more than 3.",
+      bobaBot: "5 is the maximum.",
+    },
+    2: {
+      title1: "Pick your toppings and add-ons.",
+      title2: "What skills would you like to use in a career?",
+      subtitle: "Select more than one.",
+      bobaBot: "6 is the maximum.",
+    },
+  };
+
+  const content = textContent[step] || textContent["1"];
+
+  return (
+    <Grid
+      container
+      direction="row"
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      paddingX={5}
+      sx={{
+        "@media (max-width: 1000px)": {
+          justifyContent: "center",
+          textAlign: "center",
+        },
+      }}
+    >
+      <Grid item>
+        <Grid container direction="column" display="flex">
+          <Typography variant="h3" mb={3}>
+            {content.title1}
+          </Typography>
+          <Typography variant="h3">{content.title2}</Typography>
+          <Typography variant="h5" color={CustomTheme.palette.ThaiTea.main}>
+            {content.subtitle}
+          </Typography>
+          <div style={{ height: 40 }} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        <BobaBot text={content.bobaBot} />
+      </Grid>
+    </Grid>
   );
 }
 
