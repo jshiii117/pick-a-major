8baa3754d570a02d4431f0460767556a48322573
Raymond0T <36683390+Raymond0T@users.noreply.github.com>
2023-06-06 19:48:25 -0700

    data for careers_list (#112)

--- tmt-app/src/utils/constants.js
+++ tmt-app/src/utils/constants.js
@@ -6,6 +6,8 @@ export const CAREERS_LIST = {
     name: "Accounting/Finance",
     desc: "Accounting is the process of recording, classifying, and summarizing financial transactions to provide information that is useful in making business decisions. Finance is the study of how individuals and organizations manage their money, including the processes of acquiring and using financial resources over time. Accountants and financial professionals use tools such as financial statements and spreadsheets to analyze and interpret financial data, and they may also be involved in forecasting and budgeting, risk management, and decision-making",
     link: "https://www.zippia.com/accountant-jobs/",
+    skills: ["Accounting", "Financial Reporting", "Data Management"],
+    majors: ["Computer Science & Math", "Economics & Business"],
     key: "accounting-finance",
   },
   art: {
@@ -13,6 +15,8 @@ export const CAREERS_LIST = {
     name: "Art",
     desc: "An art career often refers to a job that involves being creative or producing works of art, often using a specific medium. This can include professionals who produce their own art, like painters and sculptors, as well as jobs that entail working with art or evaluating its value. Art careers encompass a broad field of subjects, from art history to interior design to illustration. Professionals typically choose a specific concentration in college and hone their skills for that particular industry. Many art careers exist in the entertainment industry, but you can also find artistic jobs in industries like marketing, fashion and news reporting",
     link: "https://www.zippia.com/artist-jobs/",
+    skills: ["Communication", "Time Management", "Art Knowledge"],
+    majors: ["Visual Arts & Design"],
     key: "art",
   },
   consultant: {
@@ -20,6 +24,12 @@ export const CAREERS_LIST = {
     name: "Consultant",
     desc: "Consulting is the practice of providing expert advice to organizations in order to help them solve problems, improve their performance, and achieve their goals. Consultants may work in a variety of industries and may specialize in areas such as strategy, operations, finance, or technology. Consultants work with clients to understand their needs, conduct research and analysis, and develop recommendations and solutions to address their challenges. Consultants may also be involved in implementing their recommendations and helping clients to achieve their desired outcomes",
     link: "https://www.zippia.com/consultant-jobs/",
+    skills: ["Communication", "Time Management", "Research"],
+    majors: [
+      "Computer Science & Math",
+      "Economics & Business",
+      "Physical Science & Engineering",
+    ],
     key: "consultant",
   },
   "data-science": {
@@ -27,6 +37,8 @@ export const CAREERS_LIST = {
     name: "Data Science",
     desc: "Data science is the field that deals with the collection, analysis, and interpretation of large datasets. It involves using statistical and computational techniques to extract insights and knowledge from data, and applying this knowledge to solve real-world problems. Data scientists often use tools such as machine learning algorithms and programming languages like Python to analyze and interpret data",
     link: "https://www.zippia.com/data-scientist-jobs/",
+    skills: ["Data Visualization", "Statistical Analysis", "Programming"],
+    majors: ["Computer Science & Math"],
     key: "data-science",
   },
   design: {
@@ -34,6 +46,13 @@ export const CAREERS_LIST = {
     name: "Design",
     desc: "UX/UI design is the process of designing the user experience and user interface of a product, such as a website or application. It involves creating wireframes, prototypes, and user flows to understand how a user will interact with a product, and then designing the visual elements, such as the layout, color scheme, and typography, to create an intuitive and enjoyable experience for the user",
     link: "https://www.zippia.com/user-experience-designer-jobs/demographics/",
+    skills: ["Design Software", "Design Principles"],
+    majors: [
+      "Computer Science & Math",
+      "Performing Arts",
+      "Psychology & Neuroscience",
+      "Visual Arts & Design",
+    ],
     key: "design",
   },
   education: {
@@ -41,6 +60,8 @@ export const CAREERS_LIST = {
     name: "Education",
     desc: "Being a teacher involves planning and delivering lessons to students in a classroom setting, as well as grading assignments and providing feedback to help students learn and grow. Teachers also play a role in developing lesson plans, creating a positive learning environment, and fostering meaningful relationships with students. Teachers may work with students of all ages, from early childhood education to post-secondary education, and may specialize in a particular subject area or grade level",
     link: "https://www.zippia.com/teacher-jobs/",
+    skills: ["Communication", "Empathy", "Subject Knowledge"],
+    majors: ["Education", "Humanities & Cultures"],
     key: "education",
   },
   "engineering-non-software": {
@@ -48,6 +69,8 @@ export const CAREERS_LIST = {
     name: "Engineering (non-software)",
     desc: "Engineering is the application of scientific, economic, and practical knowledge in order to design, build, and maintain structures, machines, devices, systems, and processes. Engineers use math and science to solve problems and improve the world around us through the design and development of new technologies and products",
     link: "https://www.zippia.com/engineer-jobs/",
+    skills: ["Quality Control", "Troubleshooting"],
+    majors: ["Computer Science & Math", "Physical Science & Engineering"],
     key: "engineering-non-software",
   },
   "healthcare-professional": {
@@ -55,6 +78,12 @@ export const CAREERS_LIST = {
     name: "Healthcare Professional",
     desc: "Being a healthcare professional involves providing medical care and treatment to patients, often in a clinical setting such as a hospital or doctor's office. Healthcare professionals may work in a variety of roles, including doctors, nurses, and other specialized positions, and may be responsible for diagnosing and treating illnesses, administering medication, and providing preventive care and education to patients. Healthcare professionals also play an important role in advocating for the health and well-being of their patients and may be involved in research and education to advance the field of medicine",
     link: "https://www.zippia.com/health-care-provider-jobs/",
+    skills: ["Communication", "Empathy", "Technical Knowledge"],
+    majors: [
+      "Biology & Health",
+      "Humanities & Cultures",
+      "Psychology & Neuroscience",
+    ],
     key: "healthcare-professional",
   },
   hr: {
@@ -62,6 +91,18 @@ export const CAREERS_LIST = {
     name: "HR",
     desc: "Human resources (HR) is the department within a company that is responsible for managing the personnel and administrative functions of the organization. This includes tasks such as recruiting and hiring employees, managing payroll and benefits, training and developing staff, and handling employee relations and conflict resolution. HR professionals work to ensure that the company's workforce is aligned with the organization's goals and objectives and that employees are treated fairly and with respect",
     link: "https://www.zippia.com/advice/human-resources/",
+    skills: [
+      "Human Resource Management",
+      "Talent Management",
+      "Learning and Development",
+    ],
+    majors: [
+      "Economics & Business",
+      "Education",
+      "Humanities & Cultures",
+      "Psychology & Neuroscience",
+      "Social & Political Science",
+    ],
     key: "hr",
   },
   "investment-banking": {
@@ -69,6 +110,13 @@ export const CAREERS_LIST = {
     name: "Investment Banking",
     desc: "Investment banking is the division of a bank that helps clients raise capital by underwriting and issuing securities, as well as providing financial advisory services. Investment bankers work with companies, governments, and other organizations to raise the necessary capital to fund their operations, expand their businesses, or complete mergers and acquisitions. Investment bankers may also be involved in trading securities, structuring financial deals, and providing market insights and analysis to clients",
     link: "https://www.zippia.com/investment-banker-jobs/",
+    skills: [
+      "Data Interpretation",
+      "Financial Analysis",
+      "Risk Management",
+      "Physical Science & Engineering",
+    ],
+    majors: ["Computer Science & Math"],
     key: "investment-banking",
   },
   "law-legal": {
@@ -76,6 +124,13 @@ export const CAREERS_LIST = {
     name: "Law/Legal",
     desc: "Law is the system of rules and principles that a society or government recognizes and enforces through its courts and other institutions. The legal profession involves the study, interpretation, and application of these laws, as well as the representation of clients in legal matters such as litigation, contracts, and negotiations. Lawyers may work in private practice, for a government agency, or in-house for a corporation or other organization, and may specialize in areas such as criminal law, corporate law, or intellectual property law",
     link: "https://www.zippia.com/lawyer-jobs/",
+    skills: ["Information Analysis", "Public Speaking", "Writing"],
+    majors: [
+      "Economics & Business",
+      "Languages",
+      "Physical Science & Engineering",
+      "Social & Political Science",
+    ],
     key: "law-legal",
   },
   marketing: {
@@ -83,6 +138,18 @@ export const CAREERS_LIST = {
     name: "Marketing",
     desc: "Marketing is the process of identifying, anticipating, and satisfying customer needs and wants through the creation, promotion, and distribution of products and services. It involves conducting market research, developing marketing strategies and campaigns, and using various tactics, such as advertising and social media, to reach and engage with potential customers. Marketing professionals are responsible for promoting products and services to target audiences in order to increase sales and grow a company's market share",
     link: "https://www.zippia.com/marketing-manager-jobs/",
+    skills: [
+      "Copywriting",
+      "Search Engine Optimization",
+      "Customer Relationship Management",
+    ],
+    majors: [
+      "Economics & Business",
+      "Humanities & Cultures",
+      "Languages",
+      "Psychology & Neuroscience",
+      "Social & Political Science",
+    ],
     key: "marketing",
   },
   "product-management": {
@@ -90,14 +157,29 @@ export const CAREERS_LIST = {
     name: "Product Management",
     desc: "Product management is the process of defining, developing, and bringing a product to market. It involves working with a team of designers, engineers, and other stakeholders to identify the needs of the target audience, develop a product roadmap, and create a plan to bring the product to market. Product managers are responsible for overseeing the entire product lifecycle, from conception to launch and beyond, and for making strategic decisions about the direction of the product",
     link: "https://www.zippia.com/product-manager-jobs/demographics/",
+    skills: ["Market Research", "Product Vision", "Roadmapping"],
+    majors: [
+      "Computer Science & Math",
+      "Physical Science & Engineering",
+      "Visual Arts & Design",
+    ],
     key: "product-management",
   },
 
-  "project-project-management": {
+  "project-program-management": {
     id: 13,
     name: "Project/Program Management",
     desc: "Project management is the process of planning, organizing, and overseeing the work of a team to achieve specific goals and meet deadlines. It involves establishing a project plan, identifying and acquiring the necessary resources, and managing the budget, scope, and risks of the project. Project managers are responsible for ensuring that the project stays on track and is completed successfully",
     link: "https://www.zippia.com/program-manager-jobs/",
+    skills: ["Stakeholder Engagement", "Risk Management", "Change Management"],
+    majors: [
+      "Computer Science & Math",
+      "Economics & Business",
+      "Education",
+      "Humanities & Cultures",
+      "Physical Science & Engineering",
+      "Psychology & Neuroscience",
+    ],
     key: "project-program-management",
   },
   recruiting: {
@@ -105,6 +187,19 @@ export const CAREERS_LIST = {
     name: "Recruiting",
     desc: "Talent recruiting is the process of identifying and attracting qualified candidates for employment opportunities within a company. This involves conducting job postings, sourcing candidates through various channels such as job boards and social media, reviewing resumes and conducting interviews, and making hiring decisions. Talent recruiters work to build relationships with potential candidates and help connect them with job opportunities that align with their skills and experience",
     link: "https://www.zippia.com/recruiter-jobs/",
+    skills: [
+      "Communication",
+      "Applicant Tracking System Knowledge",
+      "Talent Analytics",
+    ],
+    majors: [
+      "Education",
+      "Humanities & Cultures",
+      "Languages",
+      "Performing Arts",
+      "Psychology & Neuroscience",
+      "Social & Political Science",
+    ],
     key: "recruiting",
   },
   "software-engineering": {
@@ -112,6 +207,8 @@ export const CAREERS_LIST = {
     name: "Software Engineering",
     desc: "A software engineer is responsible for designing, developing, and maintaining software systems. This involves working with a team to identify the needs of the users, writing and testing code, and ensuring that the software is efficient, reliable, and easy to use. Software engineers may also be involved in the ongoing maintenance and improvement of existing software systems",
     link: "https://www.zippia.com/software-engineer-jobs/",
+    skills: ["Programming", "Software Architecture", "Software Testing"],
+    majors: ["Computer Science & Math", "Physical Science & Engineering"],
     key: "software-engineering",
   },
 };
