93709e005f2e2ff0dfa21cf605540331b53ada3c
James Shi <jamesshi117@gmail.com>
2023-07-12 15:53:45 -0700

    Eslint fix

--- tmt-app/src/Components/QuizStepper.jsx
+++ tmt-app/src/Components/QuizStepper.jsx
@@ -1,13 +1,6 @@
-import * as React from "react";
-import { useLocation } from "react-router-dom";
-import HomeIcon from "../Images/icons/boba_progress/Home.svg";
-import SubjectsIcon from "../Images/icons/boba_progress/Subjects.svg";
-import SoftSkillsIcon from "../Images/icons/boba_progress/SoftSkills.svg";
-import OrderUpIcon from "../Images/icons/boba_progress/OrderUp.svg";
 import {
   Box,
   Container,
-  useMediaQuery,
   Step,
   StepConnector,
   stepConnectorClasses,
@@ -15,7 +8,15 @@ import {
   Stepper,
   styled,
   Typography,
-} from "@mui/material";
+  useMediaQuery,
+} from '@mui/material';
+import * as React from 'react';
+import { useLocation } from 'react-router-dom';
+
+import HomeIcon from '../Images/icons/boba_progress/Home.svg';
+import OrderUpIcon from '../Images/icons/boba_progress/OrderUp.svg';
+import SoftSkillsIcon from '../Images/icons/boba_progress/SoftSkills.svg';
+import SubjectsIcon from '../Images/icons/boba_progress/Subjects.svg';
 
 const ProgressConnector = styled(StepConnector)(({ theme }) => ({
   [`&.${stepConnectorClasses.active}`]: {
@@ -40,39 +41,39 @@ function ProgressIcon(props) {
   const icons = [
     {
       icon: <img width="64px" src={HomeIcon} alt="Home" />,
-      smallText: "Leave the Shop",
-      largeText: "Home",
+      smallText: 'Leave the Shop',
+      largeText: 'Home',
     },
     {
       icon: <img width="64px" src={SubjectsIcon} alt="Subjects" />,
-      smallText: "Pick a Base",
-      largeText: "Subjects",
+      smallText: 'Pick a Base',
+      largeText: 'Subjects',
     },
     {
       icon: <img width="64px" src={SoftSkillsIcon} alt="Soft Skills" />,
-      smallText: "Pick Tapioca",
-      largeText: "Soft Skills",
+      smallText: 'Pick Tapioca',
+      largeText: 'Soft Skills',
     },
     {
       icon: <img width="64px" src={OrderUpIcon} alt="Order Up" />,
-      smallText: "Sealed the drink",
-      largeText: "Order Up!",
+      smallText: 'Sealed the drink',
+      largeText: 'Order Up!',
     },
   ];
 
   const { icon, smallText, largeText } = icons[props.icon - 1];
 
-  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down('sm'));
 
   return (
     <Container
       sx={{
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "column",
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
         height: 50,
-        justifyContent: "center",
-        overflow: "visible",
+        justifyContent: 'center',
+        overflow: 'visible',
         zIndex: 1,
       }}
     >
@@ -80,13 +81,13 @@ function ProgressIcon(props) {
         <div>{icon}</div>
       ) : (
         <>
-          <Typography variant="caption" sx={{ textAlign: "center" }}>
+          <Typography variant="caption" sx={{ textAlign: 'center' }}>
             {smallText}
           </Typography>
           <div>{icon}</div>
           <Typography
             variant="subtitle1"
-            sx={{ fontWeight: "bold", textAlign: "center" }}
+            sx={{ fontWeight: 'bold', textAlign: 'center' }}
           >
             {largeText}
           </Typography>
@@ -99,14 +100,14 @@ function ProgressIcon(props) {
 export default function QuizStepper() {
   const location = useLocation();
   const routes = {
-    "/": 0,
-    "/quiz/classes": 1,
-    "/quiz/skills": 2,
-    "/receipt": 3,
+    '/': 0,
+    '/quiz/classes': 1,
+    '/quiz/skills': 2,
+    '/receipt': 3,
   };
 
   return (
-    <Box sx={{ my: "3rem" }}>
+    <Box sx={{ my: '3rem' }}>
       <Stepper
         alternativeLabel
         activeStep={routes[location.pathname]}
